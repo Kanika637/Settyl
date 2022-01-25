5084f2f5df7140c528f636ac3c909d1cbfb750c1
kanika637 <kanikagola26@gmail.com>
2022-01-25 21:08:11 +0530

    my first commit

--- server/Server.js
+++ server/Server.js
@@ -9,4 +9,6 @@ const port=config.port;
 const server=http.createServer(app);
 server.listen(port);
 
+// const PORT = process.env.PORT || 3333;
+
 console.log("Server is running on : 127.0.0.1:" + port);
